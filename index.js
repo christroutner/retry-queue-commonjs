f2a71e7a6b2714645a4ff5598f69e9599d389594
Chris Troutner <chris.troutner@gmail.com>
2022-09-19 08:52:01 -0700

    Forked from retry-queue ESM version

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+  An npm JavaScript library for front end web apps. Implements a minimal
+  Bitcoin Cash wallet.
+*/
+
+/* eslint-disable no-async-promise-executor */
+
+import RetryQueue from './lib/retry-queue.js'
+
+// class BoilerplateLib {
+//   constructor () {
+//     _this = this
+//
+//     _this.bchjs = new BCHJS()
+//     _this.util = util
+//   }
+// }
+
+export default RetryQueue
